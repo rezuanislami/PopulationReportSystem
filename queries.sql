a55a0c99bccc9e19875e417394a03b278918e82e
Thapa Magar <40737653@live.napier.ac.uk>
2025-03-11 11:54:38 +0000

    Added sql file

---
+++ queries.sql
@@ -0,0 +1,3 @@
+SELECT Name, Population FROM country ORDER BY Population DESC;
+SELECT Name, Population FROM city WHERE CountryCode = 'USA' ORDER BY Population DESC;
+SELECT Name, Population FROM country ORDER BY Population DESC LIMIT 10;
